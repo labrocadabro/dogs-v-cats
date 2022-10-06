735750ea6f41a8db2bb23742a860faead1bea1d9
Laura <moomal.abro@gmail.com>
2022-10-06 12:19:22 +0500

    remove unnecessary div

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -1,8 +1,8 @@
 export default function Credits({catMode}) {
   return (
 		<>
-			<p><a className={catMode ? "text-orange" : "text-purple"} href={catMode ? "https://catfact.ninja/" : "https://kinduff.github.io/dog-api/"} id="facts-link" target="_blank" rel="noreferrer"><span class="pet"></span>{catMode ? "Cat" : "Dog"} facts</a></p>
-      <p><a className={catMode ? "text-orange" : "text-purple"} href={catMode ? "https://thecatapi.com/" : "https://dog.ceo/dog-api/"} id="photos-link" target="_blank" rel="noreferrer"><span class="pet"></span>{catMode ? "Cat" : "Dog"} photos</a></p>
+			<p><a className={catMode ? "text-orange" : "text-purple"} href={catMode ? "https://catfact.ninja/" : "https://kinduff.github.io/dog-api/"} id="facts-link" target="_blank" rel="noreferrer">{catMode ? "Cat" : "Dog"} facts</a></p>
+      <p><a className={catMode ? "text-orange" : "text-purple"} href={catMode ? "https://thecatapi.com/" : "https://dog.ceo/dog-api/"} id="photos-link" target="_blank" rel="noreferrer">{catMode ? "Cat" : "Dog"} photos</a></p>
 		</>
   )
 }
