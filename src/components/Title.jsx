5b57842fa3a89dd57e8a6d396cbfd41510094b59
Laura <moomal.abro@gmail.com>
2022-10-06 09:30:09 +0500

    title complete

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,12 +1,20 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDog, faCat } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDog, faCat } from '@fortawesome/free-solid-svg-icons';
+
+import { useState, useEffect } from "react";
 
 export default function Title({catMode}) {
+	const [praise, setPraise] = useState("Rule");
+	useEffect(() => {
+    const praises = ['Rule', 'Are Awesome', 'Are Great', 'Are the Best', 'FTW', 'Forever'];
+		const random = Math.floor(Math.random() * praises.length);
+		setPraise(praises[random]);
+  }, [catMode]);
   return (
 		<h1 id="heading">
-			<FontAwesomeIcon /><FontAwesomeIcon />
-			<span class="pet"></span>s <span id="praise"></span>
-			<FontAwesomeIcon /><FontAwesomeIcon />
+			<FontAwesomeIcon icon={catMode ? faCat : faDog} /><FontAwesomeIcon icon={catMode ? faCat : faDog} />
+			{catMode ? "Cats" : "Dogs"} {praise}
+			<FontAwesomeIcon icon={catMode ? faCat : faDog} flip="horizontal" /><FontAwesomeIcon icon={catMode ? faCat : faDog} flip="horizontal" />
 		</h1>
   )
 }
